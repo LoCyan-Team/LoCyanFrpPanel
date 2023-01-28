e8e341251142ffdee1602205e586bd21de216320
daiyangcheng <daiyangcheng@outlook.com>
2023-01-28 17:05:16 +0800

    更新使用方法块，合并XiaMoHuaHuo_CN的分支

--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -21,7 +21,7 @@
     <n-grid-item span="0:3 600:2" id="item">
       <n-card title="数据报表" size="large" style="margin-bottom: 15px">
         <n-space>
-          <n-statistic label="流量" tabular-nums>
+          <n-statistic label="剩余流量" tabular-nums>
             <n-number-animation ref="TrafficRef" :from="0" :to="trafficanimation" />
             <template #suffix>
               GiB
@@ -37,6 +37,24 @@
       </n-card>
       <n-card title="使用方法">
           我把使用方法都写在首页了总没人看不见了吧？
+          <n-divider/>
+          1. 点击隧道操作中的创建隧道, 填写自己隧道的相应信息（部分高级功能请前往旧面板新建）<br/>
+          随后点击软件下载, 进入LoCyanFrpMSApp/Lastet文件夹, 随意下载一个版本<br/>
+          随后启动客户端, 选择登录后登录自己的账号<br/>
+          在转到隧道管理, 随后启动你的隧道即可<br/>
+          第一次启动请查看自己的日志文件, 确认日志文件最后一行显示 “启动成功”<br/>
+          好了, 开始享受吧<br/>
+          2. 若需要Frp的高级功能, 你可以前往旧面板配置隧道后前往软件下载页面, 进入LoCyanFrp-0.4X.X的文件夹<br/>
+          下载适合自己系统架构的软件, 随后即可自行配置<br/>
+          注意, 萌新使用此方法导致不会用的后果自行承担<br/>
+          <n-divider/>
+          3. 注意事项：<br/>
+          (1). 若因为自己的迷幻操作导致的各种问题我们没有义务给予回答<br/>
+          (2). 不要因为群里没有人回答你的问题而到处宣扬我们没素质, 在诋毁我们之前请你想想你自身有没有做到自己该做的事情<br/>
+          (3). 我们是公益性的服务, 不要把我们和某花的服务进行比较, 免费的服务无法保障稳定性等各方面的问题, 若有这方面的需求希望你可以购买专业服务器搭建服务<br/>
+          (4). 我们承诺, 我们留存的用户数据全部采用业内标准的加密格式, 而我们的数据库也采用异地的方式, 我们不会用用户的个人隐私数据开玩笑, 某些说我们盗取用户数据的人, 也请你们说话过过脑子, 不要错误引导其他用户<br/>
+          (5). 不准搭建关于赌博, 黄色, 血腥, 暴力, 爆破, 发包, 代刷网, 发卡网等有违中国法律的站点或服务, 一经发现, 我们有不在事先通知用户的情况下删除隧道, 封停账户, 将信息上报公安的权力, 不要试图挑战我们的底线<br/>
+          (6). 我们允许建设图床, 网盘等服务, 但必须建设在香港千兆服务器上, 在2023/01/28之前建立的隧道不用遵循此规定, 违规的隧道将会被强制迁移, 这是为了保障大部分用户的体验<br/>
       </n-card>
     </n-grid-item>
   </n-grid>
@@ -63,7 +81,7 @@
 </style>
 
 <script setup>
-import { NCard, NAlert, NButton, NSpace, useMessage, NGrid, NGridItem, NStatistic, NNumberAnimation } from "naive-ui";
+import { NCard, NAlert, NButton, NSpace, useMessage, NGrid, NGridItem, NStatistic, NNumberAnimation, NDivider } from "naive-ui";
 import { GetContents, GetLoginStatus, GetProxies} from "../utils/profile.js";
 import { ref } from "vue";
 import store from "../utils/store.js";
@@ -113,7 +131,7 @@ export default defineComponent({
   },
   methods: {
     getFxItemlist() {
-      // 每次访问都会执行一次status，这边刷新本地存储就好了
+      // 每次访问都会执行一次status, 这边刷新本地存储就好了
       traffic.value = Number(localStorage.getItem("traffic")) / 1024 + "GB";
       trafficanimation.value = Number(localStorage.getItem("traffic")) / 1024;
     },
