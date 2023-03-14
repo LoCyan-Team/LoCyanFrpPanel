ccff2547b13b7e200520ec84977c2b7c2690de6b
Daiyangcheng <daiyangcheng@outlook.com>
2023-03-14 10:38:15 +0800

    更新 QQ登录

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -8,8 +8,14 @@
       />
       <br />
       <n-text>{{ store.getters.GetUserName }}</n-text>
+      <br />
+      <n-h2>社交账号绑定</n-h2>
+      <n-space>
+        <n-h5 style="margin: 3px"> QQ: </n-h5>
+        <n-button type="info" @click="DoBindQQ" :loading="binding"> 绑定QQ账号 </n-button>
+      </n-space>
       <template #footer>
-        <n-button ghost round type="primary" @click="DoLogOut">退出登录</n-button>
+        <n-button ghost round type="primary" @click="DoLogOut"> 退出登录 </n-button>
       </template>
     </n-drawer-content>
   </n-drawer>
@@ -18,18 +24,29 @@
 <script setup>
 
 import { Logout } from "../utils/profile.js";
-import store from "../utils/store";
-import { NDrawer, NDrawerContent, NAvatar, NText, NButton} from "naive-ui";
+import store from "../utils/stores/store.js";
+import { NDrawer, NDrawerContent, NAvatar, NText, NButton, NH2, NH5, NSpace} from "naive-ui";
 import {SendSuccessMessage} from "../utils/message.js";
 import { ref } from "vue";
+import { get } from "../utils/request.js";
 
 const username = store.getters.GetUserName
 const Width_DiaLog = ref("30vw");
+const binding = ref(false);
 if (document.body.clientWidth <= 800) {
   Width_DiaLog.value = "75vw";
 }
 
-
+function DoBindQQ(){
+  binding.value = true;
+  const rs = get("https://api.locyanfrp.cn/OAuth/BindQQAccount?username=" + store.getters.GetUserName + "&token=" + store.getters.GetToken, []);
+  rs.then(res => {
+    if (res.status){
+      window.open(res.url);
+      binding.value = false;
+    }
+  })
+}
 
 function DoLogOut() {
   SendSuccessMessage("您已从LCF登出，感谢您的使用！")
--- src/views/login.vue
+++ src/views/login.vue
@@ -2,35 +2,42 @@
   <n-grid cols="1" item-responsive>
     <n-grid-item span="1">
       <n-form ref="formRef" :model="model" :rules="rules" label-width="auto" require-mark-placement="right-hanging"
-        size="medium" id="item">
+        size="medium" id="item" v-show="!other_login">
         <n-form-item label="用户名 / 邮箱" path="username">
           <n-input type="text" v-model:value="model.username" placeholder="用户名" @keyup.enter="login"/>
         </n-form-item>
         <n-form-item label="密码" path="password">
           <n-input type="password" v-model:value="model.password" placeholder="密码" @keyup.enter="login"/>
         </n-form-item>
-        <div style="display: flex; justify-content: flex-end">
-          <n-space>
-            <n-button type="primary" @click="login" style="margin-right: 10px;"> 登录 </n-button>
-            <n-button ghost type="primary" @click="goregister"> 没注册？</n-button>
+        <div>
+          <n-space justify="space-between">
+              <n-button type="info" @click="qqlogin" :loading="qqlogin_loading"> QQ登录 </n-button>
+            <n-space justify="end">
+              <n-button type="primary" @click="login"> 登录 </n-button>
+              <n-button ghost type="primary" @click="goregister"> 没注册？</n-button>
+            </n-space>
           </n-space>
         </div>
       </n-form>
+      <n-spin :show="other_login" description="正在进行第三方登录处理" style="display: flex;justify-content: center;margin-top: 30vh">
+      </n-spin>
     </n-grid-item>
   </n-grid>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { NFormItem, NForm, NInput, NButton, useMessage, useLoadingBar, NGrid, NGridItem, NSpace } from "naive-ui";
+import { NFormItem, NForm, NInput, NButton, useMessage, useLoadingBar, NGrid, NGridItem, NSpace, NSpin } from "naive-ui";
 import { get, getUrlKey } from "../utils/request.js";
 import router from "../router/index.js";
 import qs from 'qs';
-import store from "../utils/store.js";
+import store from "../utils/stores/store.js";
 
 const formRef = ref(null);
 const message = useMessage();
 const ldb = useLoadingBar();
+const other_login = ref(false);
+const qqlogin_loading = ref(false);
 
 const model = ref([
   {
@@ -43,6 +50,23 @@ const model = ref([
 const redirect = getUrlKey("redirect");
 if (redirect !== null){
   console.log("登录后返回" + redirect);
+}
+
+// 检查是否存在第三方登录返回值
+// 针对QQ登录的处理
+const username_qq = getUrlKey("username_qq");
+const token_qq = getUrlKey("token_qq");
+if (username_qq !== null || token_qq !== null){
+  other_login.value = true;
+  const rs = get("https://api.locyanfrp.cn/User/DoLoginByToken?username=" + username_qq + "&token=" + token_qq, []);
+  rs.then(res => {
+    if (res.status){
+      message.success("欢迎回来，指挥官！" + res.userdata.username);
+      store.commit("setToken", res.token);
+      store.commit("setUserInfo", res.userdata);
+      router.push(redirect || "/user");
+    }
+  })
 }
 
 function goregister() {
@@ -63,6 +87,16 @@ function login() {
     }
     ldb.finish();
   });
+}
+
+function qqlogin(){
+  qqlogin_loading.value = true;
+  const rs = get("https://api.locyanfrp.cn/OAuth/QQLogin", [])
+  rs.then(res => {
+    if (res.status){
+      window.location.href = res.url;
+    }
+  })
 }
 
 const rules = {
