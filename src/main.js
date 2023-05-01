4777f2bcfdc16cc6099124267b9392dd384b02a9
LiteCat <littttt87@hotmail.com>
2023-05-01 21:23:24 +0800

    载入twemojicss

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import './style.css';
+import './twemoji-amazing.css';
 import App from './App.vue';
 import message from "./components/message.vue";
 import loadingbar from "./components/loadingbar.vue";
