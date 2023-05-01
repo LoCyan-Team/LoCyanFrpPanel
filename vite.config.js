dec7e7f924c06c801b6b4abc609f03a5168a3e36
LiteCat <littttt87@hotmail.com>
2023-05-01 21:22:06 +0800

    自动引入设置

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,30 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import cssnanoPlugin from "cssnano";
 import autoprefixer from "autoprefixer";
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar'
+          ]
+        }
+      ]
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
+  ],
   server: {
     host: "0.0.0.0" 
   },
@@ -17,4 +37,4 @@ export default defineConfig({
       ]
     }
   }
-});
+})
