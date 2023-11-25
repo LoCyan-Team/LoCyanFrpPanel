44ac044a2d6290563c32abc6a1e1570802e0d416
LiteCat <littttt87@hotmail.com>
2023-11-26 01:06:43 +0800

    美化主页

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -4,18 +4,22 @@
     <div class="sign">
       <span class="fast-flicker">Lo</span>Cyan<span class="flicker">Frp</span>
     </div>
-    <n-alert type="default" style="font-size: 20px; display: inline-block">
-      一款真正完全免费的内网穿透软件！
+    <n-alert type="default" style="font-size: 20px; display: inline-block">一款真正完全免费的内网穿透软件！
     </n-alert>
     <br/><br/>
     <n-button-group>
       <n-space :x-gap="5">
         <n-button
             size="large"
-            type="primary"
-            ghost
+            strong secondary type="primary"
             @click="router.push({ name: 'Login' })"
-        >进入
+        >
+        <template #icon>
+        <n-icon>
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><path d="M13.49 5.48c1.1 0 2-.9 2-2s-.9-2-2-2s-2 .9-2 2s.9 2 2 2zm-3.6 13.9l1-4.4l2.1 2v6h2v-7.5l-2.1-2l.6-3c1.3 1.5 3.3 2.5 5.5 2.5v-2c-1.9 0-3.5-1-4.3-2.4l-1-1.6c-.4-.6-1-1-1.7-1c-.3 0-.5.1-.8.1l-5.2 2.2v4.7h2v-3.4l1.8-.7l-1.6 8.1l-4.9-1l-.4 2l7 1.4z" fill="currentColor"></path></svg>
+        </n-icon>
+      </template>
+        进入
         </n-button
         >
         <n-button size="large" @click="GoToDocs">文档</n-button>
