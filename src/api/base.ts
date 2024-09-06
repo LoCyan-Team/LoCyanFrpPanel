7959f65b8185917c785e364d854732a495109edd
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-09-07 06:38:47 +0800

    [*] Fix data response

--- src/api/base.ts
+++ src/api/base.ts
@@ -4,8 +4,13 @@ const base = {
   api_v1_url: 'https://api.locyanfrp.cn',
   api_v2_url: 'https://api-v2.locyanfrp.cn/api/v2',
   buildResponse: (res: any, useDataPath: boolean = true) => {
-    const message = res.data?.data?.message ?? res.data?.data?.msg ?? res.data?.message ?? res.data?.msg ?? '未指定或请求失败'
-    const data = useDataPath ? res.data?.data : res.data ?? {}
+    const message =
+      res.data?.data?.message ??
+      res.data?.data?.msg ??
+      res.data?.message ??
+      res.data?.msg ??
+      '未指定或请求失败'
+    const data = (useDataPath ? res.data?.data : res.data) ?? {}
     return {
       status: res.status as number,
       message: message,
