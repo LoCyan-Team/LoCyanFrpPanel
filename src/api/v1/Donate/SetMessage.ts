c4bb6b3ff60d48b877c194fbb24eb65c40d29d71
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-09-06 12:30:58 +0800

    fix prettier

--- src/api/v1/Donate/SetMessage.ts
+++ src/api/v1/Donate/SetMessage.ts
@@ -5,20 +5,14 @@ import { get } from '@/utils/request'
 /**
  * 列出赞助列表
  */
-const SetMessage = async (
-    username: string,
-    token: string,
-    trade_no: string,
-    message: string
-
-) => {
-    const rs = get(`${base.api_v1_url}/Donate/SetMessage`, {
-        username: username,
-        token: token,
-        trade_no: trade_no,
-        message: message
-    })
-    return base.buildResponse(await rs, false)
+const SetMessage = async (username: string, token: string, trade_no: string, message: string) => {
+  const rs = get(`${base.api_v1_url}/Donate/SetMessage`, {
+    username: username,
+    token: token,
+    trade_no: trade_no,
+    message: message
+  })
+  return base.buildResponse(await rs, false)
 }
 
 export default SetMessage
