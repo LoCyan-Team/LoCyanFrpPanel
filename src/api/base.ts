f42fa6579dcbb1ba02e4252ec49d93f2fbd7196e
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-09-23 02:41:09 +0800

    返回兼容

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,11 +1,14 @@
 const base = {
   api_v1_url: 'https://api.locyanfrp.cn',
   api_v2_url: 'https://api-v2.locyanfrp.cn/api/v2',
+  // api_v2_url: 'http://localhost:8080/api/v2',
   buildResponse: (res: any, useDataPath: boolean = true) => {
     const message =
-      res.data?.data?.message ??
-      res.data?.data?.msg ??
       res.data?.message ??
+      res.data?.data?.message ??
+      res.response?.data?.message ??
+      res.response?.data?.data?.message ??
+      res.response?.data?.msg ??
       res.data?.msg ??
       '未指定或请求失败'
     const data = (useDataPath ? res.data?.data : res.data) ?? {}
