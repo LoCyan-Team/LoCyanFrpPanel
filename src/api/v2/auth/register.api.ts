6c57e74fca75e6577737590ff73be820d94d35d3
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-09-29 05:45:53 +0800

    [*] 适配后端更改

--- src/api/v2/auth/register.api.ts
+++ src/api/v2/auth/register.api.ts
@@ -6,15 +6,15 @@ const register = async (
   username: string,
   password: string,
   email: string,
-  verify: number,
-  qq: number
+  verify_code: number,
+  qq_code: number
 ) => {
   const rs = post(`${base.api_v2_url}/auth/register`, {
     username: username,
     password: password,
     email: email,
-    verify: verify,
-    qq: qq
+    verify_code: verify_code,
+    qq_code: qq_code
   })
   return base.buildResponse(await rs)
 }
