17f3a1224d03dd498ceb8b001950bceb202855a9
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-12-05 22:27:30 +0800

    [+] Add some API implementation

--- src/api/v2/app/index.ts
+++ src/api/v2/app/index.ts
@@ -1,5 +1,9 @@
+import root from './root.api'
 import info from './info.api'
+import update from './update.api'
 
 export default {
-  info: info
+  root: root,
+  info: info,
+  update: update,
 }
---
+++ src/api/v2/app/update.api.ts
@@ -0,0 +1,20 @@
+//@ts-ignore
+import { post } from '@/utils/request'
+import base from '@/api/base'
+
+const update = async (
+  user_id: number,
+  app_id: number,
+  app_description: string | null,
+  redirect_url: string | null,
+) => {
+  const rs = post(`${base.api_v2_url}/app/update`, {
+    user_id: user_id,
+    app_id: app_id,
+    app_description: app_description,
+    redirect_url: redirect_url,
+  })
+  return base.buildResponse(await rs)
+}
+
+export default update
