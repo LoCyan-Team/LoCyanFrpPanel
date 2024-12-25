e600e51294e6c1bbff76692e9d37bde31d7b8a66
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-12-25 15:30:10 +0800

    [*] Fix type announce

--- src/api/v2/email/password.api.ts
+++ src/api/v2/email/password.api.ts
@@ -2,7 +2,7 @@
 import { get } from '@/utils/request'
 import base from '@/api/base'
 
-const password = async (user: number) => {
+const password = async (user: string) => {
   const rs = get(`${base.api_v2_url}/email/password`, {
     user: user
   })
